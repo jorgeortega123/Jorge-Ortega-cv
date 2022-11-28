5007b463b9673ecbf76a567ef3f28285cdb2508b
JorgeOrtega <jorgeortega@pop-os.localdomain>
2022-11-28 10:45:28 -0500

    convert Contact segment into component

--- src/components/cv/Essencials/Foooter/Contact/Contact.tsx
+++ src/components/cv/Essencials/Foooter/Contact/Contact.tsx
@@ -14,58 +14,41 @@ export default function ContactComponent({
 }) {
   const [userTextWrote, setuserTextWrote] = useState("");
   const { data, sendText } = useMainContext();
+  const dataContact = [
+    { inf: "+593 9627  16235", icon: CallSVG, title: "Call me", href:"tel:" },
+    { inf: dataText.contact.email, icon: MailSVG, title: "Email", href: "mailto:" },
+  ];
+  //          href={`${"mailto:" + dataText.contact.email}`}
+  //          href={`${"tel:" + "+593 9627  16235"}`}
+  //          onClick={() => copyToClipBoard("email")}
   return (
-    <div className="bg-transparent flex flex-col lg:flex-row rounded-[11px] w-full items-center justify-left">
-      <div>
-        <div
-          className="flex-col cursor-pointer"
-          onClick={() => copyToClipBoard("number")}
-        >
-          <div className="flex">
-            <img className="w-[36px] h-[36px] " src={CallSVG} alt="" />
-            <p>Call Me</p>
-          </div>
-          <a
-            href={`${"tel:" + "+593 9627  16235"}`}
-            className="text-[16px] pt-[5px] pl-2 w-[238px] text-center hover:text-green-400"
-          >
-            {"+593 9627  16235"}
-          </a>
-          {/* { <img
-                        className="w-[22px] h-[22px] mt-[2px]"
-                        src={CopySGV}
-                        alt=""
-                      />} */}
-        </div>
-        <div
-          className="flex-col cursor-pointer"
-          onClick={() => copyToClipBoard("email")}
-        >
-          <div className="flex">
-            <img className="w-[36px] h-[36px]" src={MailSVG} alt="" />
-            <p>Email</p>
-          </div>
-          <a
-            href={`${"mailto:" + dataText.contact.email}`}
-            className="text-[16px] pt-[5px]  w-[238px]  pl-2 underline hover:text-green-400"
-          >
-            {dataText.contact.email}
-          </a>
-        </div>
+    <div className="bg-transparent flex lg:space-x-5 flex-col lg:flex-row rounded-[11px] w-full items-center">
+      <div className="flex flex-col items-start space-y-2 ">
+        {dataContact.map((e) => {
+          return (
+            <div className="flex justify-center items-center ">
+              <div className="bg-slate-600 w-12 h-12 mx-2"><img src={e.icon} alt="" /></div>
+              <div>
+                <div className="font-bold text-[16px]">{e.title}</div>
+                <div className="font-light text-[14px]"><a href={`${e.href + e.inf}`}>{e.inf}</a></div>
+              </div>
+            </div>
+          );
+        })}
       </div>
-      <div className="relative w-full flex justify-center mt-3">
+      <div className="relative w-full flex justify-center">
         <div className="pt-12 space-y-7  w-full flex flex-col relative overflow-x-hidden overflow-y-auto items-center justify-center">
           <div className="flex space-x-3  lg:space-x-6 w-full">
-            <div className="border-[1px] flex-col input-contact w-full">
-              <p className="text-[13px] target-p-contact">Hola</p>
+            <div className="border-[2px] flex-col input-contact w-full">
+              <p className="text-[13px] target-p-contact">Name</p>
               <input className="input-sender bg-transparent  " type="text" />
             </div>
-            <div className="border-[1px] flex-col input-contact w-full">
-              <p className="text-[13px] target-p-contact">Holsa</p>
-              <input className="input-sender bg-transparent " type="text" />
+            <div className="border-[2px] flex-col input-contact w-full">
+              <p className="text-[13px] target-p-contact">Email</p>
+              <input type="email" className="input-sender bg-transparent " />
             </div>
           </div>
-          <div className="border-[1px] flex-col w-full input-contact ">
+          <div className="border-[2px] flex-col w-full input-contact ">
             <p className="text-[13px] target-p-contact">Subject</p>
             <input
               className="input-sender bg-transparent w-full input-contact"
@@ -76,7 +59,7 @@ export default function ContactComponent({
           <textarea
             id="textareOfFooter"
             placeholder={dataText.extras.footer.input}
-            className="input-contact w-full h-[200px] focus:border-cyan-500 px-2 input-sender hover:border-cyan-600 active:border-cyan-600  border-[1px] rounded-[8px]  "
+            className="input-contact w-full h-[200px] focus:border-cyan-500 px-2 input-sender hover:border-cyan-600 active:border-cyan-600  border-[2px] rounded-[8px]  "
             name=""
             onChange={(e) => {
               setuserTextWrote(e.target.value);
