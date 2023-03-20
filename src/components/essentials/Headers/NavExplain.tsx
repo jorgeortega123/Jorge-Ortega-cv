927534b73f3836ea46f762475f60b700292a2663
JorgeOrtega <jorgeortega@pop-os.localdomain>
2023-03-19 22:31:21 -0500

    optimized nav when appears

--- src/components/essentials/Headers/NavExplain.tsx
+++ src/components/essentials/Headers/NavExplain.tsx
@@ -6,43 +6,5 @@ export default function NavExplain({
   dataText,
 }) {
   if (!showMenuNavbar) return <></>;
-  return (
-    <div
-      onClick={() => {
-        document
-          .getElementById("button-nav-id")
-          ?.classList.remove("active-nav");
-        setshowMenuNavbar(false);
-      }}
-      className="h-full w-screen  absolute z-[8] bg-[#0000006b] "
-    >
-      <div className="menu-items capitalize top-0 fixed">
-        <p className="hover:text-[#66ff00] transition-all">
-          <a onClick={() => setshowMenuNavbar(false)} href="#home">
-            {dataText.headers.home}
-          </a>
-        </p>
-        <p className="hover:text-[#66ff00] transition-all">
-          <a onClick={() => setshowMenuNavbar(false)} href="#about">
-            {dataText.headers.about}
-          </a>
-        </p>
-
-        <p className="hover:text-[#66ff00] transition-all">
-          <a onClick={() => setshowMenuNavbar(false)} href="#proyects">
-            {dataText.headers.proyects}
-          </a>
-        </p>
-        <p className="hover:text-[#66ff00] transition-all">
-          <a onClick={() => setshowMenuNavbar(false)} href="#contact">
-            {dataText.headers.contact}
-          </a>
-        </p>
-        <div className="absolute bottom-0 left-2 text-[18px]">
-    
-          <p>Made with &hearts; by Jorge Ortega</p>
-        </div>
-      </div>
-    </div>
-  );
+  return <></>;
 }
--- src/components/essentials/Headers/NavView.tsx
+++ src/components/essentials/Headers/NavView.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
-import useMainContext from "../../context/useMainContext";
+
 import TranslateView from "../TranslateView";
 import AnimationsLoader from "../AnimationsLoader/AnimationsLoader";
 export default function NavView({
@@ -12,13 +12,60 @@ export default function NavView({
   children,
   dataText,
 }) {
-  const { showMenuNavbar, setshowMenuNavbar } = useMainContext();
-  
+  const [showMenuNavbar, setshowMenuNavbar] = useState(false);
+  const menu = [
+    {
+      text: "",
+      to: "",
+    },
+  ];
+
   return (
     <>
-      <div className="nav h-[46px] fixed top-0 blockAllSelect flex items-center justify-center ">
+      <div className=" nav h-[46px] fixed top-0 blockAllSelect flex items-center justify-center ">
+        {showMenuNavbar && (
+          <>
+            <div
+              onClick={() => {
+                document
+                  .getElementById("button-nav-id")
+                  ?.classList.remove("active-nav");
+                setshowMenuNavbar(false);
+              }}
+              className="anim-modal h-screen top-[46px] w-screen absolute z-[4] bg-[#0000006b]  "
+            ></div>
+            <div className=" menu-items  absolute right-0 top-[100%] z-[5] ">
+              <div className="capitalize ">
+                <p className="hover:text-[#66ff00] transition-all">
+                  <a onClick={() => setshowMenuNavbar(false)} href="#home">
+                    {dataText.headers.home}
+                  </a>
+                </p>
+                <p className="hover:text-[#66ff00] transition-all">
+                  <a onClick={() => setshowMenuNavbar(false)} href="#about">
+                    {dataText.headers.about}
+                  </a>
+                </p>
+
+                <p className="hover:text-[#66ff00] transition-all">
+                  <a onClick={() => setshowMenuNavbar(false)} href="#proyects">
+                    {dataText.headers.proyects}
+                  </a>
+                </p>
+                <p className="hover:text-[#66ff00] transition-all">
+                  <a onClick={() => setshowMenuNavbar(false)} href="#contact">
+                    {dataText.headers.contact}
+                  </a>
+                </p>
+                <div className="absolute bottom-0 left-2 text-[10px]">
+                  <p>Made with &hearts; by Jorge Ortega</p>
+                </div>
+              </div>
+            </div>
+          </>
+        )}
         <div className="w-full max-w-[800px] relative">
-          <div className="flex w-full lg:max-w-[200px]">
+          <div className="relative flex w-full lg:max-w-[200px]">
             <button
               id="button-nav-id"
               className=" button-nav visible lg:invisible "
@@ -67,32 +114,32 @@ export default function NavView({
                 )}
               </AnimatePresence>
             </div>
-            <div className="invisible h-full pr-5 flex justify-center items-center lg:visible absolute text-slate-300 right-0 useNavLetter text-[1rem] ">
-              <motion.div
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1, x: 0 }}
-                exit={{ opacity: 0 }}
-                transition={{ type: "tween" }}
-                className="capitalize flex space-x-8"
-              >
-                <p className="hover:text-white">
-                  <a href="#home">{dataText.headers.home}</a>
-                </p>
-                <p className="hover:text-white">
-                  <a href="#about">{dataText.headers.about}</a>
-                </p>
+          </div>{" "}
+          <div className=" top-0 invisible h-full pr-5 flex justify-center items-center lg:visible absolute text-slate-300 right-0 useNavLetter text-[1rem] ">
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0 }}
+              transition={{ type: "tween" }}
+              className="capitalize flex space-x-8"
+            >
+              <p className="hover:text-white">
+                <a href="#home">{dataText.headers.home}</a>
+              </p>
+              <p className="hover:text-white">
+                <a href="#about">{dataText.headers.about}</a>
+              </p>
 
-                <p className="hover:text-white">
-                  <a href="#proyects">{dataText.headers.proyects}</a>
-                </p>
-                <p className="hover:text-white">
-                  <a href="#proyects">{dataText.headers.services}</a>
-                </p>
-                <p className="hover:text-white">
-                  <a href="#contact">{dataText.headers.contact}</a>
-                </p>
-              </motion.div>
-            </div>
+              <p className="hover:text-white">
+                <a href="#proyects">{dataText.headers.proyects}</a>
+              </p>
+              <p className="hover:text-white">
+                <a href="#proyects">{dataText.headers.services}</a>
+              </p>
+              <p className="hover:text-white">
+                <a href="#contact">{dataText.headers.contact}</a>
+              </p>
+            </motion.div>
           </div>
         </div>
       </div>
